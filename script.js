8a349da9ff8bad565f2e4664b1abdc73a2a1aff8
ascodeasice <ab0988956087@gmail.com>
2022-06-27 11:10:15 +0800

    Finish validation of password and confirmation.

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const country = document.getElementById("country");
 const zipCode = document.getElementById("zipCode");
 const password = document.getElementById("password");
 const passwordConf = document.getElementById("passwordConf");
+const specialCharRegex = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
 
-function isValidUSZip(sZip) {
-  return /^\d{5}(-\d{4})?$/.test(sZip);
-}
+// function isValidUSZip(sZip) {
+//   return /^\d{5}(-\d{4})?$/.test(sZip);
+// }
 
 //validation for email
 email.addEventListener("input", () => {
-  if (email.value == "") {
+  if (email.validity.valueMissing) {
     email.setCustomValidity("You must enter email");
     email.reportValidity();
   }
@@ -20,5 +21,73 @@ email.addEventListener("input", () => {
   }
   else {
     email.setCustomValidity("");
+  }
+})
+
+country.addEventListener("input", () => {
+  if (country.validity.valueMissing) {
+    country.setCustomValidity("country is required");
+    country.reportValidity();
+  }
+  else {
+    country.setCustomValidity("");
+  }
+})
+
+zipCode.addEventListener("input", () => {
+  if (zipCode.validity.valueMissing) {
+    zipCode.setCustomValidity("Zip code is required");
+    zipCode.reportValidity();
+  }
+  else if (zipCode.validity.patternMismatch) {
+    zipCode.setCustomValidity("Invalid zip code");
+    zipCode.reportValidity();
+  }
+  else {
+    zipCode.setCustomValidity("");
+  }
+})
+
+password.addEventListener("input", () => {
+  if (password.validity.valueMissing) {
+    password.setCustomValidity("password is required");
+    password.reportValidity();
+  }
+  else if (password.validity.tooShort) {
+    password.setCustomValidity("password is too short(at least 8)");
+    password.reportValidity();
+  }
+  else if (!/\d/.test(password.value)) {
+    password.setCustomValidity("password need to contain at least one number");
+    password.reportValidity();
+  }
+  else if (!specialCharRegex.test(password.value)) {
+    password.setCustomValidity("password need to contain at least one special character");
+    password.reportValidity();
+  }
+  else if (!/[A-Z]/.test(password.value)) {
+    password.setCustomValidity("password need to contain at least one uppercase character");
+    password.reportValidity();
+  }
+  else if (!/[a-z]/.test(password.value)) {
+    password.setCustomValidity("password need to contain at least one lowercase character");
+    password.reportValidity();
+  }
+  else {
+    password.setCustomValidity("");
+  }
+})
+
+passwordConf.addEventListener("input", () => {
+  if (passwordConf.validity.valueMissing) {
+    passwordConf.setCustomValidity("You need to confirm your password");
+    passwordConf.reportValidity();
+  }
+  else if (passwordConf.value != password.value) {
+    passwordConf.setCustomValidity("The passwords don't match");
+    passwordConf.reportValidity();
+  }
+  else {
+    passwordConf.setCustomValidity("");
   }
 })
